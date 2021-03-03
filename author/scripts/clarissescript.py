d2bca72b2036c1a62ede46ac556d50444aaea612
Rico Koschmitzky <rico.koschmitzky@trixter.de>
2021-03-03 19:05:45 +0100

    add CLARISSE_SCRIPT_WRAPPER

--- author/scripts/clarissescript.py
+++ author/scripts/clarissescript.py
@@ -22,55 +22,14 @@
 #  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                 #
 # ######################################################################################################################
 
-""" The default jobtronaut configuration to show what is configurable """
+""" wrapper for executing python code when using clarisse-node --script
 
-import os
+The script needs to be called when using clarisse-node with --script flag, because no Clarisse launch mode is capable
+of executing python code directy as we would need within the current implementation of Task.script() within
+the farmsubmission API.
 
-from collections import OrderedDict
+"""
 
-LOGGING_NAMESPACE = "jobtronaut"
+import sys
 
-# some wrapper scripts that are required to allow the given DCC to call python code directly
-MAYA_SCRIPT_WRAPPER = os.path.join(os.path.dirname(__file__), "author", "scripts", "mayascript.mel")
-KATANA_SCRIPT_WRAPPER = os.path.join(os.path.dirname(__file__), "author", "scripts", "katanascript.py")
-NUKE_SCRIPT_WRAPPER = os.path.join(os.path.dirname(__file__), "author", "scripts", "nukescript.py")
-CLARISSE_SCRIPT_WRAPPER = os.path.join(os.path.dirname(__file__), "author", "scripts", "clarissescript.py")
-
-# the maximum character limit our serialized arguments string can have
-# when hitting the limit we dump the content to a file instead of passing it
-# to the command directly
-ARGUMENTS_SERIALIZED_MAX_LENGTH = 10000
-# storage path for the dumped serialized arguments
-ARGUMENTS_STORAGE_PATH = ""
-
-# a "reserved" argument we can provide to allow additional flags to a tractor commandtask through a job
-COMMANDFLAGS_ARGUMENT_NAME = "additional_command_flags"
-
-# storage path template for our jobs as alf files
-JOB_STORAGE_PATH_TEMPLATE = ""
-
-# to keep the engine user credentials more secret a function callable can be defined
-# that would return the credentials tuple (username, password)
-TRACTOR_ENGINE_CREDENTIALS_RESOLVER = lambda: ("unknown_user", "unknown_password")
-
-# The searchpaths for any kind of plugins (tasks/processors)
-PLUGIN_PATH = []
-# Whether the plugin path should only be resolved once and read from a cache for successive accesses.
-# You can use Plugins().initialize() to force a resolve of the plugin paths at any time.
-ENABLE_PLUGIN_CACHE = True
-
-# A resolver for converting a command id like `maya` into an absolute path.
-# A command id is always the first item in the list that gets returned by task.cmd()
-EXECUTABLE_RESOLVER = \
-    lambda x: x \
-        if os.path.isfile(x) and os.path.isabs(x) and os.access(x, os.X_OK) \
-        else (_ for _ in ()).throw(
-            OSError("Command Id `{}` is not an executable.".format(x))
-        )
-
-# A way to resolve an environment. It will be assumed that the resolver would return a dict or OrderedDict
-# where the key represents the env var name and the value a string joining all values separated through the
-# required separator character
-ENVIRONMENT_RESOLVER = lambda: OrderedDict(sorted(os.environ.items()))
-# Only if True the resolver will be used and the environment be passed to the envkey attribute of the resulting job
-INHERIT_ENVIRONMENT = True
+exec(sys.argv[1])
