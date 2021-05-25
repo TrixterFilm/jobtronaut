2550b57fd2872ad04e49c2ee7422ad8df9852696
Rico Koschmitzky <rico.koschmitzky@trixter.de>
2021-05-25 15:32:44 +0200

    add missing copyright header

--- author/TractorSiteStatusFilter.py
+++ author/TractorSiteStatusFilter.py
@@ -1,5 +1,5 @@
 # ######################################################################################################################
-#  Copyright 2020-2021 TRIXTER GmbH                                                                                         #
+#  Copyright 2020-2021 TRIXTER GmbH                                                                                    #
 #                                                                                                                      #
 #  Redistribution and use in source and binary forms, with or without modification, are permitted provided             #
 #  that the following conditions are met:                                                                              #
